a4d2d1655d8d1e528ad55fe03754da921e0c8b8a
Kaspa-maker <55349572+Kaspa-maker@users.noreply.github.com>
2024-01-22 09:05:49 +0000

    Assets
    Added gun asset bullet and flashlight. Started on the weapon script

---
+++ Assets/_Scripts/Shooting.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float bulletLifeTime;
+
+    void Update()
+    {
+
+    }
+}
